fb6a2e255cfbecbefebec6c3f64019f403374e9f
Melanie Rodas <mrodasl@uni.pe>
2023-05-29 16:22:07 -0500

    added all cards in the section

---
+++ src/dataController.js
@@ -0,0 +1,10 @@
+import data from "./data/pokemon/pokemon.js";
+
+export const allPokemon = data.pokemon;
+export const example = () => {
+  return "example";
+};
+
+export const anotherExample = () => {
+  return "OMG";
+};
