8ee5dbbdaa21b4696d5799f4d83170c2844a7f8c
Melanie Rodas <mrodasl@uni.pe>
2023-05-31 15:04:31 -0500

    added a function called filterByType

--- src/pages/pokedex.js
+++ src/pages/pokedex.js
@@ -1,6 +1,4 @@
-import { example, allPokemon } from "../dataController.js";
-
-console.log(example, allPokemon);
+import { allPokemon } from "../dataController.js";
 
 function createCard(pokemon) {
   let allPills = "";
@@ -26,3 +24,35 @@ const cards = document.getElementById("cards");
 for (let i = 0; i < allPokemon.length; i++) {
   cards.innerHTML += createCard(allPokemon[i]);
 }
+
+//Filter by Type
+const newPokemon = [...allPokemon];
+
+function filterByType(newPokemon, type) {
+  const result = newPokemon.filter(function (pokemon) {
+    return pokemon.type.includes(type);
+  });
+  return result;
+}
+
+console.log("function filter-electric", filterByType(newPokemon, "electric"));
+
+console.log("function filter-water", filterByType(newPokemon, "water"));
+console.log("function filter-flying", filterByType(newPokemon, "flying"));
+
+/*order by number
+function orderByNumber(newArray, direction) {
+  const orderedArray = newArray.sort((a, b) => {
+    if (direction === "asc") {
+      return a.num > b.num ? 1 : -1;
+    } else if (direction === "desc") {
+      return a.num < b.num ? 1 : -1;
+    }
+  });
+  return orderedArray;
+}
+
+console.log("function sort-asc", orderByNumber(newArray, "asc"));
+console.log("function sort-des", orderByNumber(newArray, "desc"));
+*/
+console.log("un solo pokemon", newPokemon[0].type);
